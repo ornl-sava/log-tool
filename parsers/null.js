c426eeec0387194f1093411178040c33c1045304
euf <euf@ornl.gov>
2012-07-23 13:22:06 -0400

    fixed configs and null parser

--- parsers/null.js
+++ parsers/null.js
@@ -2,13 +2,7 @@
 //exports.start = function(opts){}
 
 exports.data = function(data){
-  //this.emit('data', data)
-  var lines = data.split('\n');
-  //NB: for real parsers, don't do this, use es.split( same regex you already have ) or es json equiv.
-  for(line in lines){
-    this.emit('data', lines[line] + '\n')
-    this.emit('data', 'and also ' + lines[line] + '\n')
-  }
+  this.emit('data', data)
 }
 
 exports.end = function(){
