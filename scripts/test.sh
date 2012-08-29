106f1b5960770461914b1c612b3a4487d2243f74
euf <euf@ornl.gov>
2012-08-29 15:33:18 -0400

    better tests for file-watch

--- scripts/test.sh
+++ scripts/test.sh
@@ -25,8 +25,21 @@ diff in.txt out.txt
 echo '' > out.txt
 node ./test/stdoutTest.js > out.txt
 diff in.txt out.txt
+
+touch temp.in.txt
+rm temp.in.txt
+touch temp.in.txt
+node ./test/watchfileTest.js &
+sleep 1
+cat in.txt > temp.in.txt
+sleep 1
+diff in.txt out.txt
+
 echo 'done!'
 echo ''
 
 echo 'running mocha tests ...'
 mocha -u tdd -R spec -t 10000 ./test/mocha-tests.js
+
+rm ./out.txt
+rm ./temp.in.txt
---
+++ test/watchfileTest.js
@@ -0,0 +1,43 @@
+"use strict";
+
+// Import opts command line options parser module
+//  https://bitbucket.org/mazzarelli/js-opts/wiki/Home
+var opts = require('opts');
+
+var core = require('../lib/core.js')
+
+var opts = {}
+
+opts.inputConfig = {
+  "watch":{
+    "module":"file-watch",
+    "fileName":"temp.in.txt",
+    "timeout": 3000,
+    "interval": 100,
+    "encoding":"utf-8"
+  }
+}
+opts.outputConfig = {
+  "out.txt":{
+    "module":"file",
+    "fileName":"out.txt"
+  }
+}
+opts.parserConfig = {
+  "null":{
+    "module":"null"
+  }
+}
+opts.connectionConfig = [
+  {
+    "input":"watch",
+    "parser":"null",
+    "output":"out.txt"
+  }
+]
+
+var instance = new core.LogTool(opts)
+
+instance.on('done', process.exit) //TODO best way to do this?  How come it sometime exists without needing this (eg. for file streams)?
+
+//instance.stop()
