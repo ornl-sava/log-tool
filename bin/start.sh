4d9775379a7496b04c79b990d81ff1fdaf572dad
euf <euf@ornl.gov>
2012-09-16 19:01:23 -0400

    removed forever and scripts using forever - not useful.  moved remaining items in bin dir

--- bin/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Script must be run from the project root directory (not the scripts directory)
-
-PATH=/usr/local/bin:/usr/local/sbin:$PATH
-export PATH
-
-# Start application as daemon using [forever](https://github.com/nodejitsu/forever)
-# switch node environment for express and socket.io settings (development or production)
-./node_modules/forever/bin/forever start logtool.js
--- bin/stop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Script must be run from the project root directory (not the scripts directory)
-
-PATH=/usr/local/bin:/usr/local/sbin:$PATH
-export PATH
-
-# Shutdown application
-forever="./node_modules/forever/bin/forever"
-$forever stop logtool.js
